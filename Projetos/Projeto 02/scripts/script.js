a8ba08b9a1628d149d1772ce8d422c1c926b7b18
Bruno Valdson G dos Santos <bruno.valdson@outlook.com.br>
2023-03-31 19:16:59 -0300

    Update projeto churrascometro

--- Projetos/Projeto 02/scripts/script.js
+++ Projetos/Projeto 02/scripts/script.js
@@ -4,46 +4,95 @@
 
 // Crianças valem por 0,5
 
-let inputAdultos = document.getElementById("adultos");
-let inputCriancas = document.getElementById("criancas");
-let inputDuracao = document.getElementById("duracao");
-
-let resultado = document.getElementById("resultado");
-
-function calcular() {
-    console.log("Calculando...");
+// funcão clicar nos botões 
 
 
-    let adultos = inputAdultos.value;
-    let criancas = inputCriancas.value;
-    let duracao = inputDuracao.value;
+window.onload = function () {
 
-    let qtdTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
-    let qtdTotalCerveja = cervejaPP(duracao) * adultos
-    let qtdTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+    var column = document.getElementById("column-2");
+    // column.innerHTML += `style="margin: 0px; height: 200px;"`
 
-    resultado.innerHTML = `<p>${qtdTotalCarne/1000}Kg de Carne</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja/355)} Latas de Cerveja</p>`
-    resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas/2000)} Garrafas de 2L de Refrigerante</p>`
-}
-function carnePP(duracao) {
-    if (duracao >= 6) {
-        return 650;
-    }else{
-        return 400
+    var el = document.getElementById("qtdConvidados");
+
+    el.addEventListener('click', function (e) {
+        var element = e.target.id;
+
+        console.log(element);
+
+        let qtdDuracao = parseInt(document.getElementById("qtdDuracao").innerText);
+        let qtdCrianca = parseInt(document.getElementById("qtdCrianca").innerText);
+        let qtdAdultos = parseInt(document.getElementById("qtdAdulto").innerText);
+
+        let duracao = document.getElementById("qtdDuracao");
+        let criancas = document.getElementById("qtdCrianca");
+        let adulto = document.getElementById("qtdAdulto");
+
+        switch (element) {
+            case "btnPlus-adulto":
+                qtdAdultos++;
+                adulto.innerText = qtdAdultos;
+                break;
+            case "btnSub-adulto":
+                if (qtdAdultos > 0) {
+                    qtdAdultos--;
+                    adulto.innerText = qtdAdultos;
+                }
+                break;
+            case "btnPlus-crianca":
+                qtdCrianca++;
+                criancas.innerText = qtdCrianca;
+                break;
+            case "btnSub-crianca":
+                if (qtdCrianca > 0) {
+                    qtdCrianca--;
+                    criancas.innerText = qtdCrianca;
+                }
+                break;
+            case "btnPlus-duracao":
+                qtdDuracao++;
+                duracao.innerText = qtdDuracao;
+                break;
+            case "btnSub-duracao":
+                if (qtdDuracao > 0) {
+                    qtdDuracao--;
+                    duracao.innerText = qtdDuracao;
+                }
+                break;
+            default:
+                break;
+        }
+
+        let resultado = document.getElementById("resultado");
+
+        let qtdTotalCarne = carnePP(duracao) * adulto + (carnePP(duracao) / 2 * criancas);
+        let qtdTotalCerveja = cervejaPP(duracao) * adulto
+        let qtdTotalBebidas = bebidasPP(duracao) * adulto + (bebidasPP(duracao) / 2 * criancas);
+
+        resultado.innerHTML = `<p>${qtdTotalCarne / 1000}Kg de Carne</p>`
+        resultado.innerHTML += `<p>${Math.ceil(qtdTotalCerveja / 355)} Latas de Cerveja</p>`
+        resultado.innerHTML += `<p>${Math.ceil(qtdTotalBebidas / 2000)} Garrafas de 2L de Refrigerante</p>`
+    });
+
+    function carnePP(duracao) {
+        if (duracao >= 6) {
+            return 650;
+        } else {
+            return 400
+        }
     }
-}
-function cervejaPP(duracao) {;
-    if (duracao >= 6) {
-        return 2000;
-    }else{
-        return 1200
+    function cervejaPP(duracao) {
+        ;
+        if (duracao >= 6) {
+            return 2000;
+        } else {
+            return 1200
+        }
     }
-}
-function bebidasPP(duracao) {
-    if (duracao >= 6) {
-        return 1500;
-    }else{
-        return 1000
+    function bebidasPP(duracao) {
+        if (duracao >= 6) {
+            return 1500;
+        } else {
+            return 1000
+        }
     }
 }
