9025dd7f3db7a19b27331590b677ac14a5460a83
Bruno Valdson G dos Santos <bruno.valdson@outlook.com.br>
2023-04-25 20:00:25 -0300

    Create script.js

---
+++ Desafios/Desafio01 - Currículo/script.js
@@ -0,0 +1,31 @@
+var numero = 0;
+var timer;
+var h1;
+
+window.onload = function(){
+    let h = titulo();
+}
+function titulo(){
+    let nome = localStorage.getItem("nome");
+    h1 = document.getElementById("h1");
+    h1.innerHTML = nome;
+}
+function sortear(){
+    let np = 100;
+
+    let ns = Math.floor(Math.random() * np);
+
+    document.getElementById("h1").innerHTML = ns;
+}
+
+function iniciar(){
+    let h = document.getElementById("h1");
+    h.innerHTML =  "Sorteando..."
+
+    timer = setTimeout(sortear,1000);
+}
+function cancelar(){
+
+    clearTimeout(timer);
+    let h = titulo();
+}
