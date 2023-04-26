4a49d279ffd6b9f8738de1843f94dc05d062ea18
Bruno Valdson G dos Santos <bruno.valdson@outlook.com.br>
2023-04-25 22:55:51 -0300

    Canvas

--- Aulas/script.js
+++ Aulas/script.js
@@ -1,40 +1,10 @@
-var numero = 0;
-var timer;
-var h1;
+let tela = document.getElementById("tela");
 
-window.onload = function(){
-    let h = titulo();
-}
-function titulo(){
-    let nome = localStorage.getItem("nome");
-    h1 = document.getElementById("h1");
-    h1.innerHTML = nome;
-}
-// function atualizar(e){
-//     let valor  = e.value;
-//     console.log(valor);
+let ctx = tela.getContext("2d");
 
-//     let h1 = document.getElementById("h1");
-//     h1.innerHTML = valor;
-
-//     localStorage.setItem("nome", valor);
-// }
-function sortear(){
-    let np = 100;
-
-    let ns = Math.floor(Math.random() * np);
-
-    document.getElementById("h1").innerHTML = ns;
-}
-
-function iniciar(){
-    let h = document.getElementById("h1");
-    h.innerHTML =  "Sorteando..."
-
-    timer = setTimeout(sortear,1000);
-}
-function cancelar(){
-
-    clearTimeout(timer);
-    let h = titulo();
-}
+ctx.moveTo(0,0);
+ctx.lineTo(250,250);
+ctx.lineTo(500,0);
+ctx.lineWidth = 3;
+ctx.strokeStyle = "green"
+ctx.stroke();
