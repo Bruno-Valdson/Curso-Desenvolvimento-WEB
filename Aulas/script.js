9a468d76b6924ffd1eb3aba0515883a7d86e6b2f
Bruno Valdson G dos Santos <bruno.valdson@outlook.com.br>
2023-05-01 18:13:57 -0300

    Update script.js
    Aulas de Canvas

--- Aulas/script.js
+++ Aulas/script.js
@@ -2,9 +2,71 @@ let tela = document.getElementById("tela");
 
 let ctx = tela.getContext("2d");
 
-ctx.moveTo(0,0);
-ctx.lineTo(250,250);
-ctx.lineTo(500,0);
-ctx.lineWidth = 3;
-ctx.strokeStyle = "green"
-ctx.stroke();
+let img = new Image();
+img.src = "../Desafios/Desafio03 - Receita/images/logo_tudogostoso.png";
+
+
+img.onload = desenharImg;
+
+function desenharImg() {
+    ctx.drawImage(this, 10, 10);
+}
+
+let circle = {
+    x: 250,
+    y: 240,
+    raio: 100,
+    inicio: 0,
+    fim: 0,
+}
+
+
+function drawCircle(c) {
+
+    ctx.beginPath();
+    ctx.rect(0, 0, 500, 500);
+    ctx.fillStyle = "rgb(114, 114, 114)";
+    ctx.fill();
+
+    ctx.beginPath();
+
+    ctx.rect(10, 140, 100, 200);
+    ctx.fillStyle = "white";
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "green"
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "white";
+    ctx.fillStyle = "green";
+    ctx.moveTo(120, 140);
+    ctx.lineTo(120, 340);
+    ctx.lineTo(180, 340);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.beginPath();
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "green";
+    ctx.fillStyle = "white";
+    ctx.arc(c.x, c.y, c.raio, c.inicio, c.fim);
+
+    ctx.fill();
+    ctx.stroke();
+}
+
+setInterval(function () {
+
+    if (circle.fim < 2 * Math.PI) {
+        circle.fim += 0.3;
+        circle.x += 6;
+    }
+
+    drawCircle(circle);
+
+}, 35);
